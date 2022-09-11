c2b91365e7da592c0b6dbf6d91b65eea9f107374
Neo Chen <tim839172010@gmail.com>
2022-09-11 16:32:18 -0700

    Revert "Revert "AdminPage with public header and footer""
    This reverts commit c1e5bf2180a904360d46eef95da385078cce219b.

---
+++ AntraShare/src/app/admin/user-lists/user-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-lists',
+  templateUrl: './user-lists.component.html',
+  styleUrls: ['./user-lists.component.sass']
+})
+export class UserListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
