379f33d9298bbed09d95f2f7df5c946218ae2156
Neo Chen <tim839172010@gmail.com>
2022-09-11 15:31:52 -0700

    AdminPage with public header and footer

--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AdminModule } from './admin/admin.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
