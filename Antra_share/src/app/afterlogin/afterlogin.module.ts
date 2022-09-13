79c50ecd5465e8434607146a8a5a943091f4a6bf
Junye Li <lijunye18@gmail.com>
2022-09-13 14:56:24 -0400

    create basic files structure

---
+++ Antra_share/src/app/afterlogin/afterlogin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AfterloginRoutingModule } from './afterlogin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AfterloginRoutingModule
+  ]
+})
+export class AfterloginModule { }
---
+++ Antra_share/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
+import { AfterloginModule } from './afterlogin/afterlogin.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { LoginModule } from './login/login.module';
+import { ProfileModule } from './profile/profile.module';
+import { SettingModule } from './setting/setting.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule,
+    AfterloginModule,
+    CoreModule,
+    LoginModule,
+    ProfileModule,
+    SettingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
