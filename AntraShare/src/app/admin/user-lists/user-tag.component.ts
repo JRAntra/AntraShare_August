d4e4657d1133ad969b82d35297140da05bcf23d1
Neo Chen <tim839172010@gmail.com>
2022-09-14 13:45:54 -0700

    import global material

---
+++ AntraShare/src/app/admin/user-lists/user-tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-tag',
+  templateUrl: './user-tag.component.html',
+  styleUrls: ['./user-tag.component.sass']
+})
+export class UserTagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
