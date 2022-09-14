0260818353db05df46ca651bb3a86228a8f3b71c
ercheung3 <ercheung3@gmail.com>
2022-09-14 11:59:29 -0400

    Added route for login.

--- AntraShare/src/app/app-routing.module.ts
+++ AntraShare/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomepageComponent
+  },
+  {
+    path: "login",
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  { path: "**", redirectTo: "redirectTo"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
