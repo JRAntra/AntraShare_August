1711015e4c56a7aec6536a1551c2fb9e82327ba3
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-09-13 23:09:26 -0400

    setting up settings and profile component

--- Antra_share/src/app/setting/setting.module.ts
+++ Antra_share/src/app/setting/setting.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingRoutingModule } from './setting-routing.module';
+import { SettingsPageComponent } from './settings-page/settings-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SettingsPageComponent
+  ],
   imports: [
     CommonModule,
     SettingRoutingModule
