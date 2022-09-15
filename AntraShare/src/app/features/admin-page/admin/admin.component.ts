cbb586829790b7f44449fd2c9f6b75889b16ec9d
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2022-09-14 17:51:27 -0700

    [AA-28 RW] Added mock up admin data with ngFor to write out the userlist, and its elements  will only show if condition "showlist" is true

--- AntraShare/src/app/features/admin-page/admin/admin.component.ts
+++ AntraShare/src/app/features/admin-page/admin/admin.component.ts
@@ -11,5 +11,34 @@ export class AdminComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  public showlist = true;
+
+  userslist = [
+    {
+      name: 'Piebaconcookie',
+      email: 'richardwu90@gmail.com',
+      function: 'Delete'
+    },
+    {
+      name: 'Banana',
+      email: 'banana@slug.com',
+      function: 'Delete'
+    },
+    {
+      name: 'Minneapolis',
+      email: 'notBurntToRuins@hotmail.com',
+      function: 'Delete'
+    },
+    {
+      name: 'Ice Cream',
+      email: 'icecream@soda.com',
+      function: 'Delete'
+    },
+    {
+      name: 'Xx69DemonSlayer420xX',
+      email: 'myParentsDontLoveMe@gmail.com',
+      function: 'Delete'
+    }
+  ]
 
 }
