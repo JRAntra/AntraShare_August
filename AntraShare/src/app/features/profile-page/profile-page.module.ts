7f57d217774619f3a73c75dd0cb1be31540db1f6
zoeyzehuixiao <zoeyxiao98@gmail.com>
2022-09-14 22:21:58 -0400

    styled profile page

--- AntraShare/src/app/features/profile-page/profile-page.module.ts
+++ AntraShare/src/app/features/profile-page/profile-page.module.ts
@@ -4,7 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ProfilePageRoutingModule } from './profile-page-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     ProfileComponent
@@ -12,7 +13,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     ProfilePageRoutingModule,
-    SharedModule
+    SharedModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class ProfilePageModule { }
