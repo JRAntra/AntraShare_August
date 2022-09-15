baba4a22b345e3aa69ef635da31c92eebbfa0bfe
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-09-15 13:00:10 -0400

    initialise profile and settings page

--- Antra_share/src/app/profile/profile.module.ts
+++ Antra_share/src/app/profile/profile.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
-
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  declarations: [ProfilePageComponent],
+  imports: [CommonModule, ProfileRoutingModule],
+  exports: [ProfilePageComponent],
 })
-export class ProfileModule { }
+export class ProfileModule {}
