8a8aeb29cff7ee56095df7e060dff0277c9def0b
Hai <haiwinexcd@gmail.com>
2022-09-17 16:03:04 -0500

    added registerNow directive

---
+++ AntraShare/src/app/features/login-page/login/register-now.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RegisterNowDirective } from './register-now.directive';
+
+describe('RegisterNowDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RegisterNowDirective();
+    expect(directive).toBeTruthy();
+  });
+});
