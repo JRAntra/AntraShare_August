f9c882899c5ae489c9214ea2e1aa08fa2a977461
zoeyzehuixiao <zoeyxiao98@gmail.com>
2022-09-17 16:14:46 -0400

    displayed responses from server

--- AntraShare/src/app/shared/model/Post.ts
+++ AntraShare/src/app/shared/model/Post.ts
@@ -1,6 +1,10 @@
 export interface Post {
     publisherName: string;
-    content: object;
+    content: {
+        text: string;
+        image: string;
+        video: string;
+    };
     publishedTime: string;
     comment: object[];
     likedIdList: object[];
