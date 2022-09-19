0f52b6feb5cb1e7ba653d288263811abd10047cf
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-09-19 08:33:49 -0400

    routing complete

--- Antra_share/src/app/afterlogin/afterlogin.module.ts
+++ Antra_share/src/app/afterlogin/afterlogin.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AfterloginRoutingModule } from './afterlogin-routing.module';
-import { AfterloginComponent } from './afterlogin.component';
 import { AfterloginPageComponent } from './afterlogin-page/afterlogin-page.component';
 
-
 @NgModule({
-  declarations: [
-    AfterloginComponent,
-    AfterloginPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AfterloginRoutingModule
-  ]
+  declarations: [AfterloginPageComponent],
+  imports: [CommonModule, AfterloginRoutingModule],
 })
-export class AfterloginModule { }
+export class AfterloginModule {}
