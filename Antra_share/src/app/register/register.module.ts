566bde606049f60c89b84cdfba20aa7e646ab6e1
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-09-19 13:17:31 -0400

    register added

---
+++ Antra_share/src/app/register/register.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { RegisterRoutingModule } from './register-routing.module';
+
+@NgModule({
+  declarations: [RegisterPageComponent],
+  imports: [CommonModule, RegisterRoutingModule],
+  exports: [RegisterPageComponent],
+})
+export class RegisterModule {}
