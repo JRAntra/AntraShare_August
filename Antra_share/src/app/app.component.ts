f450fc7d4cf161e1858b2a097550af5a886b9ab1
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-09-19 10:43:17 -0400

    added directive

--- Antra_share/src/app/app.component.ts
+++ Antra_share/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  displayIndicator = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onNavigatingTo(dest: string) {
+    this.router.navigateByUrl(dest);
+  }
+
   title = 'Antra_share';
 }
