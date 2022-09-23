89cd54766b786702fd5ce33a88e865a894bd1431
Neo Chen <tim839172010@gmail.com>
2022-09-23 09:05:22 -0700

    implement the like list

--- AntraShare/src/app/homepage/services/storylist.service.ts
+++ AntraShare/src/app/homepage/services/storylist.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Post } from 'src/app/shared/models/post';
+import { Post, LikeList } from 'src/app/shared/models/post';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,21 @@ export class StorylistService {
 
   constructor(private httpClient: HttpClient) { }
   
+  likedList: LikeList[] = [];
+
   getStoryListsFromServer(): Observable<Post[]> {
     return this.httpClient.get<Post[]>("http://localhost:4231/api/news");
+  }
+
+  getLikeList(): Observable<LikeList[]> {
+    return of(this.likedList);
+  }
+
+  addPostToLikeList(likedPost: LikeList) {
+    this.likedList.push(likedPost);
+  }
+
+  deletePostFromLikeList(postId: string) {
+    this.likedList = this.likedList.filter(post => post._id !== postId);
   }
 }
--- AntraShare/src/app/homepage/story/story.component.ts
+++ AntraShare/src/app/homepage/story/story.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { StorylistService } from '../services/storylist.service';
-import { Post } from '../../shared/models/post';
+import { LikeList, Post, Content } from '../../shared/models/post';
 
 @Component({
   selector: 'app-story',
@@ -11,8 +11,18 @@ export class StoryComponent implements OnInit {
 
   constructor(private stroyListService : StorylistService) { }
 
+  @Input() post? : Post;
+
   ngOnInit(): void {
   }
 
-  @Input() post? : Post;
+  likePost() {
+    const _post: LikeList = {
+      publisherName: this.post?.publisherName as string,
+      publishedTime: this.post?.publishedTime as string,
+      content: this.post?.content as Content,
+      _id: this.post?._id as string
+    };
+    this.stroyListService.addPostToLikeList(_post);
+  }
 }
