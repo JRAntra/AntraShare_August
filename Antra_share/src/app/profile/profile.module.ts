69147351ce3ea16eac81941e9185be2a9f783b4f
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-09-24 13:44:17 -0400

    commit

--- Antra_share/src/app/profile/profile.module.ts
+++ Antra_share/src/app/profile/profile.module.ts
@@ -2,10 +2,17 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile/profile.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    MatFormFieldModule,
+    MatSliderModule,
+  ],
   exports: [ProfileComponent],
 })
 export class ProfileModule {}
