b630a21692137826844c81428203b5e2f86388b8
Hai <haiwinexcd@gmail.com>
2022-09-24 15:19:42 -0500

    added username/password validation

--- AntraShare/src/app/features/login-page/login/login.component.ts
+++ AntraShare/src/app/features/login-page/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { checkValidate } from 'src/app/shared/validators/checkUsername';
+import { ValidateLoginService } from 'src/app/shared/service/validate-login.service';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +14,13 @@ export class LoginComponent implements OnInit {
   constructor(
     private router : Router,
     private fb: FormBuilder,
+    private service: ValidateLoginService,
   ) { }
 
   userPanel = this.fb.group({
     username: new FormControl('',[
       Validators.required,
+      checkValidate(this.service),
     ]),
     password: new FormControl('',[
       Validators.minLength(8),
@@ -29,16 +33,18 @@ export class LoginComponent implements OnInit {
   }
 
 
+
+
   onNavigateTo(dest: string) {
     this.router.navigateByUrl(dest)
   }
 
-  get password() {
-    return this.userPanel.get("password")
+  get password(): FormControl {
+    return this.userPanel.get("password") as FormControl
   }
 
-  get username() {
-    return this.userPanel.get("username")
+  get username(): FormControl {
+    return this.userPanel.get("username") as FormControl
   }
 
 
