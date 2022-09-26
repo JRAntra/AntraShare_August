90bc0212e30e27652581c48f5cd1a729fc3dc9c5
richardwu900 <111394858+richardwu900@users.noreply.github.com>
2022-09-26 15:28:24 -0700

    deleting this, cleaning request, don't need this in release
    deleting this, cleaning request, don't need this in release

--- AntraShare/src/app/shared/pipes/limit-char.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LimitCharPipe } from './limit-char.pipe';
-
-describe('LimitCharPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LimitCharPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
