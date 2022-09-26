b6749a092ed36c7c9e901068db91fcad5abbffcf
richardwu900 <111394858+richardwu900@users.noreply.github.com>
2022-09-26 15:30:54 -0700

    cleaning request, making it match release

--- AntraShare/src/app/shared/shared.module.ts
+++ AntraShare/src/app/shared/shared.module.ts
@@ -1,26 +1,54 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { SharedRoutingModule } from './shared-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AutohideDirective } from './directives/autohide.directive';
-import { LimitCharPipe } from './pipes/limit-char.pipe';
+import { ModalWindowComponent } from './components/modal-window/modal-window.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+
+import { ChartsModule } from './modules/charts/charts.module';
+import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
+import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
+// import { DataTableModule } from './modules/data-table/data-table.module';
+// import { ComponentLoaderComponent } from './components/component-loader/component-loader.component';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from '@shared/interceptors/jwt.interceptor';
+// import { ErrorInterceptor } from '@shared/interceptors/error.interceptor';
+import { UserIdleTimerService } from './services/user-idle-timer.service';
+import { PasswordStrengthMeterComponent } from './components/password-strength-meter/password-strength-meter.component'
 
 @NgModule({
-  declarations: [
-    NavbarComponent,
-    AutohideDirective,
-    LimitCharPipe
+  declarations: [HeaderComponent, ModalWindowComponent, PaginationComponent, FileUploaderComponent, AutocompleteComponent, PasswordStrengthMeterComponent,
+    // ComponentLoaderComponent
   ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    RouterModule,
+    ChartsModule,
+    FormsModule
+    // DataTableModule
   ],
-  exports: [
-    NavbarComponent,
-    AutohideDirective,
-    LimitCharPipe
+  providers: [
+    UserIdleTimerService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    // { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    // fakeBackendProvider
+  ],
+  exports: [HeaderComponent, ModalWindowComponent,
+    PaginationComponent, ChartsModule, FileUploaderComponent,
+    AutocompleteComponent,
+    PasswordStrengthMeterComponent
+    // DataTableModule,
+    //ComponentLoaderComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+  constructor(private userIdleTimerService: UserIdleTimerService) {
+    
+  }
+}
