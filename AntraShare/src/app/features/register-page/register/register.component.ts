f2166c168ad604e51cfe6669adaff4608091ae06
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2022-09-27 17:46:03 -0700

    [AA-51 RW] Added input box in register page, keyup will make post request for registering new user.

--- AntraShare/src/app/features/register-page/register/register.component.ts
+++ AntraShare/src/app/features/register-page/register/register.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, ValidationErrors, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 // import { ValidateLoginService } from 'src/app/shared/service/validate-login.service';
 import { ValidateRegisterService } from 'src/app/shared/service/validate-register.service';
-import { noDuplicate} from 'src/app/shared/validators/register-validators'
+import { UserProfile } from 'src/app/shared/models/userprofile';
 import {noupper, nolower, nospecial, checkValidateEmail, checkPasswords, emailValidator, usernameValidator, confirmPasswordMatch} from 'src/app/shared/validators/register-validators'
 
 @Component({
@@ -19,6 +19,8 @@ export class RegisterComponent implements OnInit {
   public yes = true;
 
   public confirmFail = false;
+
+  public hasErrors = true;
   
   constructor(
     private router : Router,
@@ -49,21 +51,70 @@ export class RegisterComponent implements OnInit {
       Validators.required,
 
     ])
-  }, { validators: checkPasswords })
+  })
   ngOnInit(): void {
     // initialize user
 
   }
+  get username(){
+    return this.userPanel.get("username");
+  }
+  get email(){
+    return this.userPanel.get("email");
+  }
+  get password(){
+    return this.userPanel.get("password");
+  }
+  get passwordConfirm(){
+    return this.userPanel.get("passwordConfirm");
+  }
+  get userpanel(){
+    return this.userPanel;
+  }
+
+  // ripped from https://stackoverflow.com/questions/40680321/get-all-validation-errors-from-angular-2-formgroup
+  getFormValidationErrors() {
+    var hasErrors = false;
+    Object.keys(this.userPanel.controls).forEach(key => {
+      const controlErrors: ValidationErrors = this.userPanel.get(key)?.errors!;
+      if (controlErrors != null) {
+        hasErrors = true;
+        Object.keys(controlErrors).forEach(keyError => {
+         console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+        });
+      }
+    });
+    return hasErrors;
+  }
 
   onInput() {
     this.userPanel.controls["email"].setAsyncValidators(emailValidator.asyncValidateEmail(this.validateServiceEmail))
-    console.log(this.email?.errors);
+    // console.log(this.email?.errors);
 
     this.userPanel.controls["username"].setAsyncValidators(usernameValidator.asyncValiddateUsername(this.validateServiceEmail))
 
     this.userPanel.controls["passwordConfirm"].setValidators(
       confirmPasswordMatch.confirmPassword(this.userPanel.controls["password"] as FormControl)
     )
+
+    // var userName = this.username?.value;
+
+    if(this.getFormValidationErrors()) {
+
+      this.hasErrors = true;
+    } else {
+      this.hasErrors = false;
+    }
+
+    // if (this.username?.errors) {
+    //   console.log('errors in password');
+    // } else {
+    //   console.log('we good with password');
+    // }
+
+    // if (this.userPanel.controls["email"]){
+
+    // }
 
     
     if (this.userPanel.errors?.['confirmFail']) {
@@ -75,6 +126,26 @@ export class RegisterComponent implements OnInit {
     // this.userPanel.controls["username"].setAsyncValidators(profileValidators.asyncValiddateName(this.validateServiceEmail))
     // console.log(this.userPanel.controls["username"].errors)
     // console.log(this.username?.errors);
+  }
+
+  onSubmitButtonClick() {
+    console.log("lol button is enabled");
+    const profile : UserProfile = {
+      name: "hardCodedName",
+      // userName: {text : this.userPanel.controls["username"].value},
+      userName: this.username?.value!,
+      userEmail: this.email?.value!,
+      password: this.password?.value!,
+
+      userRole: "hardCodedUserRole",
+      age: 123,
+      gender: "hardCodedGender",
+      phone: 911
+    }
+    this.validateServiceEmail.registerUser(profile).subscribe(
+      //TODO: handle response
+      res => {console.log(res)}
+    )
   }
 
   // showError(key: string): string {
@@ -93,22 +164,6 @@ export class RegisterComponent implements OnInit {
   //   }
   //   return "";
   // }
-
-  get username(){
-    return this.userPanel.get("username");
-  }
-  get email(){
-    return this.userPanel.get("email");
-  }
-  get password(){
-    return this.userPanel.get("password");
-  }
-  get passwordConfirm(){
-    return this.userPanel.get("passwordConfirm");
-  }
-  get userpanel(){
-    return this.userPanel;
-  }
 
   get errorsMag() : any{
     const arr = []
