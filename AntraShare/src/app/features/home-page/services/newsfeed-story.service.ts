3e1a40e739eeebae6376942c0ab3dc859ea04da4
richardwu900 <111394858+richardwu900@users.noreply.github.com>
2022-10-03 18:33:14 -0700

    Update newsfeed-story.service.ts

--- AntraShare/src/app/features/home-page/services/newsfeed-story.service.ts
+++ AntraShare/src/app/features/home-page/services/newsfeed-story.service.ts
@@ -17,3 +17,22 @@ export class NewsfeedStoryService {
   postStory(story: NewsfeedStory) : Observable<NewsfeedStory>{
     return this.http.post<NewsfeedStory>("http://localhost:4231/api/news", story)
   }
+  
+  updateStoryList() {
+    this.http.get<NewsfeedStory[]>("http://localhost:4231/api/news").subscribe(values => {
+      this.storyList = values
+      this.storySubject.next(this.storyList.reverse())
+    })
+  }
+
+  addToLikeList(likedPost: NewsfeedStory) {
+    console.log(likedPost)
+    this.likedList.push(likedPost)
+    this.likeListSubject.next(this.likedList)
+  }
+
+  deleteFromLikeList(postId: string | undefined) {
+    this.likedList = this.likedList.filter(post => post._id !== postId)
+    this.likeListSubject.next(this.likedList)
+  }
+}  
